a8ed8ffaedbacee5a72e87da5e5d20e7ace49844
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2024-11-29 03:56:36 +0200

    fixed the windows character utf thing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <locale>
 #include "Stack.h"
 #include "SudokuBoard.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 #define RESET "\033[0m"
 #define RED "\033[31m"
@@ -8,6 +13,9 @@
 
 int main()
 {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
     SudokuBoard s;
     s.start();
     return 0;
